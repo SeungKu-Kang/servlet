985d483e99c90d914b0898fcbb18d245f3cf6177
DESKTOP-HBIBUUM\user <test@test.com>
2024-05-28 18:34:30 +0900

    주석 추가

--- src/main/java/com/test/lesson01/GetMethodEx03.java
+++ src/main/java/com/test/lesson01/GetMethodEx03.java
@@ -14,6 +14,7 @@ public class GetMethodEx03 extends HttpServlet{
 	@Override
 	public void doGet (HttpServletRequest request , HttpServletResponse response) throws IOException {
 		
+		// 테스트용 주석 추가 240528
 		response.setCharacterEncoding("utf-8");
 		response.setContentType("text/json");
 		
